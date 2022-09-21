d66c2b24abcc469d202e32ae64fe3e451580aa4c
leemyou <limhyeon0809@gmail.com>
2022-09-21 17:24:37 +0900

    Setting: react 초기 세팅

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./Style/App.css"
+import Table from "./Component/Table";
+import tableData from './Data/Data.json'
 
 function App() {
+  console.log(tableData.one);
+
+  // 학년별 분류하기  // 배열 형태
+  const grade1 = tableData.one;
+  // const grade2 = tableData.two;
+  // const grade3 = tableData.three;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='title'>
+        <h1>front-end 개발실습 과제</h1>
+        <h3>202045060 임효현</h3>
+      </div>
+
+      <Table grade="1" data={grade1}/>
     </div>
   );
 }
