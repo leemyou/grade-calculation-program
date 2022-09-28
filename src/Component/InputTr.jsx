e8035d86da652f5d28d401f953840f687c6dac8d
leemyou <limhyeon0809@gmail.com>
2022-09-28 20:39:40 +0900

    Feat&Refactore: 학점마다 보이는 페이지 div 변경
    - 코드 리펙토링
    - 1학점일때와 2,3학점일 때 보이는 성적 입력칸을 다르게 조정

--- src/Component/InputTr.jsx
+++ src/Component/InputTr.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+// import styled from "styled-components";
 import $ from 'jquery';
 
 
@@ -15,10 +16,24 @@ const InputTr = (props) => {
     inputFinValue: ''
   })
 
+
   const { inputCompleteValue, inputEssentialValue,
     inputSubjectValue, inputCreditValue,
     inputAttendValue, inputReportValue,
     inputMidValue, inputFinValue } = inputData;
+
+  const [display, setDisplay] = useState(true); // 학점에 따른 패논패 보이는 state 관리
+  
+  const CreditChange = (e) => {
+    const creditValue = e.target.value;
+    // eslint-disable-next-line eqeqeq
+    if(creditValue == 1){
+      setDisplay(true)
+    }else{
+      setDisplay(false)
+    }
+  }
+
 
   const dataChange = (e) => {
     // const {name, value} = e.target
@@ -28,53 +43,24 @@ const InputTr = (props) => {
     })
   }
 
-  // 학점 입력 제한
-  $(document).on('keyup', '.write_number_credit',function() {
-    var val= $(this).val();
-
-    if(val.replace(/[0-9]/g, "").length > 0) {
-        alert("숫자만 입력해 주십시오.");
-        $(this).val('');
-    }
-
-    if(val < 1 || val >= 4) {
-        alert("1~3학점 사이로 입력해 주십시오.");
-        $(this).val('');
-    }
-  })
-  
-  // 출석 입력 제한
-  $(document).on('keyup', '.write_number_attend',function() {
-    var val= $(this).val();
-
-    if(val.replace(/[0-9]/g, "").length > 0) {
-        alert("숫자만 입력해 주십시오.");
-        $(this).val('');
-    }
-
-    if(val < 0 || val > 20) {
-        alert("0~10점 사이로 입력해 주십시오.");
-        $(this).val('');
-    }
-  })
 
   // 과제 점수 입력 제한
-    // 출석 입력 제한
-    $(document).on('keyup', '.write_number_report',function() {
-      var val= $(this).val();
-  
-      if(val.replace(/[0-9]/g, "").length > 0) {
-          alert("숫자만 입력해 주십시오.");
-          $(this).val('');
-      }
-      if(val < 0 || val > 20) {
-          alert("0~20점 사이로 입력해 주십시오.");
-          $(this).val('');
-      }
-    })
+  // 출석 입력 제한
+  $(document).on('keyup', '.write_number_20',function() {
+    var val= $(this).val();
+
+    if(val.replace(/[0-9]/g, "").length > 0) {
+        alert("숫자만 입력해 주십시오.");
+        $(this).val('');
+    }
+    if(val < 0 || val > 20) {
+        alert("0~20점 사이로 입력해 주십시오.");
+        $(this).val('');
+    }
+  })
 
   // 시험점수 입력 제한
-  $(document).on('keyup', '.write_number_test',function() {
+  $(document).on('keyup', '.write_number_30',function() {
     var val= $(this).val();
   
     if(val.replace(/[0-9]/g, "").length > 0) {
@@ -103,23 +89,42 @@ const InputTr = (props) => {
           <option value='필수' key='필수'>필수</option>
         </select>
       </td>
-      <td>
+      <td display='true'>
         <input type='text' id='inputSubject' name='inputSubjectValue' value={inputSubjectValue} onChange={dataChange}></input>
       </td>
       <td>
-        <input type='text' className="write_number_credit" id='inputCredit' name='inputCreditValue' value={inputCreditValue} onChange={dataChange}/>
+        <select name='inputCreditValue' id='inputCredit' vlaue={inputCreditValue} onChange={(e) => {dataChange(e); CreditChange(e);}}>
+          <option value='1' key='1'>1</option>
+          <option value='2' key='2'>2</option>
+          <option value='3' key='3'>3</option>
+        </select>
       </td>
       <td>
-        <input type='text' className="write_number_attend" id='inputAttend' name='inputAttendValue' value={inputAttendValue} onChange={dataChange}/>
+        {
+          !display && <input display={display} type='text' className="write_number_20 display-none" id='inputAttend' name='inputAttendValue' value={inputAttendValue} onChange={dataChange}/>
+        }
+        {
+          display && <select display={!display} className='display-none' name="inputAttendValue" id="inputAttend" value={inputAttendValue} onChange={dataChange} style={{width: '100%'}}>
+          <option value='p' key='p'>p</option>
+          <option value='np' key='np'>np</option>
+        </select>
+        }
+        
       </td>
       <td>
-        <input type='text' className="write_number_report" id='inputReport' name='inputReportValue' value={inputReportValue} onChange={dataChange}/>
+        {
+          !display && <input type='text' display={display} className="write_number_20 display-none" id='inputReport' name='inputReportValue' value={inputReportValue} onChange={dataChange}/>
+        }
       </td>
       <td>
-        <input type='text' className="write_number_test" id='inputMid' name='inputMidValue' value={inputMidValue} onChange={dataChange}/>
+        {
+          !display && <input type='text' display={display} className="write_number_30 display-none" id='inputMid' name='inputMidValue' value={inputMidValue} onChange={dataChange}/>
+        }
       </td>
       <td>
-        <input type='text' className="write_number_test" id='inputFin' name='inputFinValue' value={inputFinValue}  onChange={dataChange}/>
+        {
+          !display && <input type='text' display={display} className="write_number_30 display-none" id='inputFin' name='inputFinValue' value={inputFinValue}  onChange={dataChange}/>
+        }
       </td>
       <td></td>
       <td></td>
