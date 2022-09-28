41f00a68a19c4cb40a6f8a8ff10e0500618a1a5e
leemyou <limhyeon0809@gmail.com>
2022-09-28 20:38:05 +0900

    Style: 참고사항 메시지 추가

--- src/App.js
+++ src/App.js
@@ -21,6 +21,15 @@ function App() {
       <Tables grade="1" header={headerData} data={grade1}/>
       <Tables grade="2" header={headerData} data={grade2}/>
       <Tables grade="3" header={headerData} data={grade3}/>
+
+      <div className="notesReference">
+        <h5>✔️ 참고 사항</h5>
+        <ol>
+          <li>성적은 출결 20, 과제 20, 중간 30, 기말 30만 입력됨.</li>
+          <li>체크박스에 체크한 후 삭제버튼을 누르면 삭제 가능.</li>
+          <li>새로고침을 하면 변경사항이 초기화.</li>
+        </ol>
+      </div>
     </div>
   );
 }
